aaabbd7e52cb68982417ebbff26e798c4edd6c72
MirkoColo <colombomirko06@gmail.com>
2023-11-27 09:51:48 +0100

    Aggiungere i file di progetto.

---
+++ EsercitazioneDiplomati/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EsercitazioneDiplomati
+{
+    class Diplomati
+    {
+        protected string _Nome { get; set; }
+        protected string _Cognome { get; set; }
+        protected int _Voto { get; set; }
+
+        public Diplomati(string Nome, string Cognome, int Voto) 
+        { 
+            _Nome = Nome;
+            _Cognome = Cognome;
+            _Voto = Voto;
+        }
+
+        public void Registrazione()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
