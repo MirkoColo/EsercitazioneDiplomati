481bcc01d9e7a38ae79666c85f80de3c1f06105b
MirkoColo <colombomirko06@gmail.com>
2023-11-28 11:12:33 +0100

    Diplomati

--- EsercitazioneDiplomati/Program.cs
+++ EsercitazioneDiplomati/Program.cs
@@ -1,36 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace EsercitazioneDiplomati
 {
     class Diplomati
     {
-        protected string _Nome { get; set; }
-        protected string _Cognome { get; set; }
-        protected int _Voto { get; set; }
+        protected string _nome;
+        protected string _cognome;
+        protected int _voto;
 
-        public Diplomati(string Nome, string Cognome, int Voto) 
-        { 
-            _Nome = Nome;
-            _Cognome = Cognome;
-            _Voto = Voto;
+        public void SetNome(string nome)
+        {
+            _nome = nome;
         }
 
-        public void Registrazione()
+        public void SetCognome(string cognome)
         {
+            _cognome = cognome;
+        }
 
+        public void SetVoto(int voto)
+        {
+            if (voto >= 36 && voto <= 100) this._voto = voto;
+            else Console.WriteLine("Il voto deve essere compreso tra 36 e 100");
+              
+        }
+
+        public void Stampa(Diplomati[] myArr, int counter)
+        {
+            for(int i = 0; i < counter; i++)
+            {
+                Console.WriteLine(myArr[i]._nome +" "+ myArr[i]._cognome + " " +myArr[i]._voto);
+            }
+        }
+
+        public void StampaAbili(Diplomati[] myArr, int counter)
+        {
+            for (int i = 0; i < counter; i++)
+            {
+                //VECCHI
+                if (myArr[i]._voto >= 36 && myArr[i]._voto <= 60)
+                {
+                    if(myArr[i]._voto >= 42)
+                    {
+                        Console.WriteLine($"Il vecchio diplomato {myArr[i]._nome} {myArr[i]._cognome} con un voto di {myArr[i]._voto} è abile ai concorsi");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Il vecchio diplomato {myArr[i]._nome} {myArr[i]._cognome} con un voto di {myArr[i]._voto} non è abile ai concorsi");
+                    }
+                    
+                }
+                else //NUOVI
+                {
+                    if(myArr[i]._voto >= 70)
+                    {
+                        Console.WriteLine($"Il nuovo diplomato {myArr[i]._nome} {myArr[i]._cognome} con un voto di {myArr[i]._voto} è abile ai concorsi");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Il nuovo diplomato {myArr[i]._nome} {myArr[i]._cognome} con un voto di {myArr[i]._voto} non è abile ai concorsi");
+                    }
+                }
+            }
         }
     }
-
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            int voto = 0;
+            int counter = 0;
+            Diplomati[] diplomati = new Diplomati[100];
+            for (int i = 0; i < 2; i++)
+            {
+                Diplomati dip = new Diplomati();
+                Console.WriteLine("Inserire il Nome: ");
+                dip.SetNome(Console.ReadLine());
+                Console.WriteLine("Inserire il Cognome: ");
+                dip.SetCognome(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("Inserire il Voto: ");
+                    voto = int.Parse(Console.ReadLine());
+                } while (voto < 36 || voto > 100);
+                dip.SetVoto(voto);                          
+                diplomati[i] = dip;
+                counter++;
 
+            }
+            Diplomati dip1 = new Diplomati();
+            dip1.Stampa(diplomati, counter);
+            dip1.StampaAbili(diplomati, counter);
+            Console.ReadLine();
         }
     }
 }
